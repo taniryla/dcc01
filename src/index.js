64447f58d4b6add0b59f882c5b3a321cfb197189
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:27:41 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+// Your solution for 01-addOne here:
+
+</div>
+`;
